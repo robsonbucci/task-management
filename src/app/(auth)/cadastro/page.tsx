9ba8f42646d93e5a14313e385581261d6e078083
robsonbucci <robsonbucci@hotmail.com>
2024-10-20 12:28:35 -0300

    feat: pagina de cadastro criada

--- src/app/(auth)/cadastro/page.tsx
+++ src/app/(auth)/cadastro/page.tsx
@@ -1,24 +1,48 @@
 "use client";
 import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
-
+import { Button, Form, Input } from "antd";
+import { useState } from "react";
 import Link from "next/link";
+
 import LoginCard from "@/app/components/LoginCard/LoginCard";
+import { useGlobalState } from "@/app/context/globalContextProvider";
 
 export default function Cadastro() {
-  const onFinish = (values: any) => {
-    console.log("Success:", values);
-  };
+  const { theme } = useGlobalState();
+  const [error, setError] = useState<string | null>(null);
 
+  const onFinish = async (values: any) => {
+    try {
+      const response = await fetch("/api/auth/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const data = await response.json();
+      if(data.status !== 201){
+        setError(data.message);
+      }
+      
+    } catch (error) {
+      setError((error as Error).message);
+    }
+  };
+  
   const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
+    setError("Erro ao realizar o cadastro. Tente novamente!");
   };
 
   return (
     <LoginCard title="Crie sua Conta">
       <Form
         name="basic"
-        labelCol={{ span: 8 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
@@ -27,29 +51,67 @@ export default function Cadastro() {
       >
         {/* Campo para Nome */}
         <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Por favor insira seu nome!" }]}
+          name="name"
+          rules={[{ required: true, message: "Insira seu nome!" }]}
         >
-          <Input prefix={<UserOutlined />} placeholder="Seu nome" />
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="Seu nome"
+          />
         </Form.Item>
 
         {/* Campo para E-mail */}
         <Form.Item
           name="email"
           rules={[
-            { required: true, message: "Por favor insira seu e-mail!" },
+            { required: true, message: "Insira seu e-mail!" },
             { type: "email", message: "O formato do e-mail é inválido!" },
           ]}
         >
-          <Input prefix={<MailOutlined />} placeholder="Seu e-mail" />
+          <Input
+            prefix={<MailOutlined />}
+            placeholder="Seu e-mail"
+          />
         </Form.Item>
 
         {/* Campo para Senha */}
         <Form.Item
           name="password"
-          rules={[{ required: true, message: "Por favor insira sua senha!" }]}
+          rules={[{ required: true, message: "Insira sua senha!" }]}
         >
-          <Input.Password prefix={<LockOutlined />} placeholder="Sua senha" />
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Sua senha"
+          />
+        </Form.Item>
+
+        {/* Campo para Confirmar Senha */}
+        <Form.Item
+          name="confirm"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Confirme sua senha!",
+            },
+            ({ getFieldValue }) => ({
+              // Logica para validar confirmação de senha
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("As duas senhas não são iguais!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Confirme sua senha"
+          />
         </Form.Item>
 
         {/* Botão de Cadastro */}
@@ -59,8 +121,10 @@ export default function Cadastro() {
           </Button>
         </Form.Item>
 
+        {error && <p className="text-red-500">{error}</p>}
+
         {/* Link para Login */}
-        <Form.Item className="text-white">
+        <Form.Item style={{ color: theme.colorFontPrimary }}>
           <Link href="/login">Já possui conta?</Link>
         </Form.Item>
       </Form>
