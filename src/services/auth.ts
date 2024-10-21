9cb33bc0f72d7ce8b20146883c562a1c7659af36
robsonbucci <robsonbucci@hotmail.com>
2024-10-20 23:08:25 -0300

    feat: criando token via JWT

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,54 +1,86 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
 
 import errorHandler from "@/utils/errorHandler";
 
 interface User {
-  id: number;
   name: string;
   email: string;
   password: string;
 }
 
-let users: User[] = [];
+const prisma = new PrismaClient();
 const SECRET: any = process.env.JWT_SECRET;
 
 function createToken(user: User) {
-  return jwt.sign({ email: user.email, name: user.name }, SECRET);
+  return jwt.sign({ email: user.email, name: user.name }, SECRET, {
+    expiresIn: "1h",
+  });
 }
 
 function verifyToken(token: string) {
   try {
     return jwt.verify(token, SECRET);
   } catch (error) {
-    throw new Error("Token inválido");
+    throw errorHandler(401, "Token inválido");
   }
 }
 
-export function cadastroUser(userData: User) {
-  const user = users.find((user) => user.email === userData.email);
+export function validateToken(token: string) {
+  const user = verifyToken(token);
+  return user;
+}
+
+export async function cadastroUser(userData: User) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: userData.email,
+    },
+  });
+
   if (user) {
     throw errorHandler(409, "Usuario já cadastrado!");
   }
 
-  users.push(userData);
+  // Criptografar a senha antes de salvar
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+  // Salvar o usuario com a senha criptografada
+  const newUser = await prisma.user.create({
+    data: {
+      name: userData.name,
+      email: userData.email,
+      password: hashedPassword,
+    },
+  });
+
   const token = createToken(userData);
-  return token;
+  return {status: 201, token};
 }
 
-export function loginUser(userData: Partial<Pick<User, "email" | "password">>) {
-  const user = users.find(
-    (user) =>
-      user.email === userData.email && user.password === userData.password
-  );
+export async function loginUser(
+  userData: Partial<Pick<User, "email" | "password">>
+) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: userData.email,
+    },
+  });
 
   if (!user) {
     throw errorHandler(404, "Usuario não encontrado!");
   }
 
-  if (user.password !== userData.password) {
+  const isPasswordValid = await bcrypt.compare(
+    userData.password!,
+    user.password
+  );
+
+  if (!isPasswordValid) {
     throw errorHandler(401, "Usuario ou senha incorretos!");
   }
 
   const token = createToken(user);
-  return token;
+  return {status: 200, token};
 }
